8a7e25fc25532ff314796cee8d8fa7df145aa7a1
KeenTraan <tranmanhphong3103@gmail.com>
2022-12-07 10:13:46 +0700

    initial file

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    navlink: [
+      {name: "Home", path: '/'},
+      {name: "Todo", path: '/todo'},
+      {name: "Example 1", path: '/example1'},
+      {name: "Example 2", path: '/example2'}
+    ]
   },
   getters: {
   },
