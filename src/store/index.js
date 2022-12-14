2a7ac3027d9bdd16dae7d82ca65bf0d13db39c6f
KeenTraan <tranmanhphong3103@gmail.com>
2022-12-14 08:49:47 +0700

    update file feature_change_status

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { createStore } from "vuex";
-import ModulesTodos  from "@/store/modules/ModuleTodo";
+import Todo  from "@/store/modules/Todo";
 export default createStore({
   state: {},
   getters: {},
   mutations: {},
   actions: {},
   modules: {
-    ModulesTodos,
+    Todo,
   },
 });
