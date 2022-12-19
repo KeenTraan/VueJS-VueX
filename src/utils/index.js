c1023f543571d346a8f2efc50b6efc699f411700
KeenTraan <tranmanhphong3103@gmail.com>
2022-12-19 14:18:50 +0700

    fix bug format time

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,5 +9,8 @@ export function getFormatTime(time) {
   const year = newTime.getFullYear();
   const hour = newTime.getHours();
   const minute = newTime.getMinutes();
+  if (minute < 10) {    
+    return `${hour}:0${minute} ${date}/${month}/${year}`;
+  }
   return `${hour}:${minute} ${date}/${month}/${year}`;
 }
